988ed17fef91b3ae2e32c4571cf2cd0ad1956590
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-19 16:44:41 +0100

    updated home.tsx

--- src/template/views/home.tsx
+++ src/template/views/home.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid"; // Import Grid component
 import { BasePageProps } from "@src/utils/interface";
 import { DEFAULT_LOGO, queryClient } from "@src/utils";
 import CourseExamLeaguePublicationWrapper from "@src/components/Layout/Wrapper/CourseExamLeaguePublicationWrapper";
@@ -15,9 +21,42 @@ const LeaguePage = () => {
       showHeader={true}
       showFooter={true}
     >
-      <CourseExamLeaguePublicationHomePage />
+      <div className={classes.hero}>
+        <Container maxWidth="md">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              {/* Left column */}
+              <Typography variant="h3" className={classes.heroTitle}>
+                Welcome to {name} Academy
+              </Typography>
+              <Typography variant="subtitle1" className={classes.heroSubtitle}>
+                Explore our courses, exams, leagues, and publications. Join us on a journey of knowledge!
+              </Typography>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.heroButton}
+                onClick={() => alert("Button Clicked")}
+              >
+                Get Started
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {/* Right column with the image */}
+              <img
+                src="/images/hero-img.png" // Replace with your image path
+                alt="School Image"
+                className={classes.image}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+      <Container maxWidth="md">
+        <CourseExamLeaguePublicationHomePage />
+      </Container>
     </CourseExamLeaguePublicationWrapper>
   );
 };
 
-export default LeaguePage;
+export default Home;
