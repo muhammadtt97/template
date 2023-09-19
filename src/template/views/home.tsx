ca7d84036f65d23d71bce518b993cd274ad7cacb
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-09-19 16:45:01 +0100

    Update home.tsx

--- src/template/views/home.tsx
+++ src/template/views/home.tsx
@@ -9,7 +9,45 @@ import { DEFAULT_LOGO, queryClient } from "@src/utils";
 import CourseExamLeaguePublicationWrapper from "@src/components/Layout/Wrapper/CourseExamLeaguePublicationWrapper";
 import CourseExamLeaguePublicationHomePage from "@src/components/CourseExamLeaguePublicationHome";
 
-const LeaguePage = () => {
+const useStyles = makeStyles((theme) => ({
+  hero: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    padding: theme.spacing(8, 0),
+    textAlign: "center",
+    borderRadius: "0 0 20px 20px",
+  },
+  heroTitle: {
+    fontWeight: "bold",
+    marginBottom: theme.spacing(2),
+    textShadow: `2px 2px 4px ${theme.palette.primary.dark}`,
+  },
+  heroSubtitle: {
+    marginBottom: theme.spacing(4),
+    fontSize: "1.2rem",
+  },
+  heroButton: {
+    marginTop: theme.spacing(2),
+    backgroundColor: theme.palette.secondary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  },
+  logo: {
+    maxWidth: "100%",
+    height: "auto",
+  },
+  // Style for the image
+  image: {
+    width: "100%",
+    height: "auto",
+    borderRadius: "20px", // Rounded corners for the image
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+
   const { cachedData } = queryClient.getQueryData("pageProps") as BasePageProps;
   const { name, logo } = cachedData.centre;
 
